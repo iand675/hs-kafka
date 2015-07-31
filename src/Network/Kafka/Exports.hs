51be71414520bec6fd0a2de6b50a5fa4aa43703d
Ian Duncan <ian@iankduncan.com>
2015-07-31 10:08:28 -0400

    More ByteSize instances

--- src/Network/Kafka/Exports.hs
+++ src/Network/Kafka/Exports.hs
@@ -1,13 +1,17 @@
+{-# LANGUAGE RankNTypes #-}
 module Network.Kafka.Exports
 ( module Data.Binary
 , module Data.Int
-, module GHC.Generics
 , module GHC.TypeLits
 , module Network.Kafka.Fields
 , ByteSize(..)
+, byteSizeL
+, Generic
 ) where
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BL
 import Data.Int
-import GHC.Generics
+import GHC.Generics hiding (to)
 import GHC.TypeLits
 import Data.Binary
 import Network.Kafka.Fields
@@ -15,6 +19,9 @@ import Network.Kafka.Fields
 -- | Note that for requests and responses, only NON-COMMON FIELDS should be counted in the instance
 class ByteSize a where
   byteSize :: a -> Int32
+
+instance ByteSize Int8 where
+  byteSize = const 1
 
 instance ByteSize Int16 where
   byteSize = const 2
@@ -25,3 +32,12 @@ instance ByteSize Int32 where
 instance ByteSize Int64 where
   byteSize = const 8
 
+instance ByteSize BS.ByteString where
+  byteSize = fromIntegral . BS.length
+
+instance ByteSize BL.ByteString where
+  byteSize = fromIntegral . BL.length
+
+byteSizeL :: ByteSize a => Getter s a -> s -> Int32
+byteSizeL g = view (g . to byteSize)
+{-# INLINE byteSizeL #-}
